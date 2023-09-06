cd42878da8d22b8025f9d78e8986d01bb1dcc1c5
Glaucoed <gesverzuthe@gmail.com>
2023-09-06 11:59:28 -0300

    feat: criado area dos planos

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -4,29 +4,29 @@ export default function Banner() {
   return (
     <>  
     <div className="bg-[url('/banner.jpg')] h-24 lg:h-64 bg-cover bg-center border-b" /> 
-      <div className="h-full bg-white mx-auto w-3/5 lg:w-2/5 rounded-2xl mt-[-25px] mb-12 flex flex-col gap-4">
+      <div className="h-full bg-white mx-auto w-3/5 lg:w-2/3 rounded-2xl mt-[-25px] mb-12 flex flex-col gap-4">
 
           <h1 className="text-xl lg:text-3xl font-bold text-center text-bege-800 mt-2">Arena Carioca</h1>
           <h2 className="text-sm lg:text-sm text-center">Aqui você encontra Esportes de Areia | Gastronomia & Drinks | Shows</h2>
 
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
-            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-44 h-36 flex flex-col justify-between items-center">
-              <Image src="/beachtennis.jpg" width={120} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
+            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-56 h-36 flex flex-col justify-between items-center">
+              <Image src="/beachtennis.jpg" width={200} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
               <h2 className="lg:text-lg text-sm text-white font-semibold">Beach Tennis</h2>
               <button className="bg-white text-black px-4 py-1 text-xs rounded-lg font-semibold ">Agende-já</button>
             </div>
-            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-44 h-36 flex flex-col justify-between items-center">
-              <Image src="/voleipraia.jpg" width={120} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
+            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-56 h-36 flex flex-col justify-between items-center">
+              <Image src="/voleipraia.jpg" width={200} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
               <h2 className="lg:text-lg text-sm text-white font-semibold">Vôlei de Praia</h2>
               <button className="bg-white text-black px-4 py-1 text-xs rounded-lg font-semibold">Agende-já</button>
             </div>
-            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-44 h-36 flex flex-col justify-between items-center">
-              <Image src="/gastro.jpg" width={120} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
+            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-56 h-36 flex flex-col justify-between items-center">
+              <Image src="/gastro.jpg" width={200} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
               <h2 className="lg:text-lg text-sm text-white font-semibold">Gastronomia</h2>
               <button className="bg-white text-black px-4 py-1 text-xs rounded-lg font-semibold">Cardapio</button>
             </div>
-            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-44 h-36 flex flex-col justify-between items-center">
-              <Image src="/shows.jpg" width={120} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
+            <div className="bg-bege-800 rounded-lg shadow-md p-2 lg:h-56 h-36 flex flex-col justify-between items-center">
+              <Image src="/shows.jpg" width={200} height={60} alt="raquete" className="rounded-lg transition duration-300 ease-in-out hover:scale-105"/>
               <h2 className="lg:text-base text-xs text-white font-semibold">Shows e Eventos</h2>
               <button className="bg-white text-black  px-4 py-1 text-xs rounded-lg font-semibold">Veja mais</button>
             </div>
--- src/app/components/Plans.tsx
+++ src/app/components/Plans.tsx
@@ -1,66 +1,100 @@
-
+import { FaCheck } from 'react-icons/fa'
 export default function Plans() {
   return (
     <div className="h-full p-4">
       <h2 className="text-2xl text-center text-bege-800 font-bold mb-4">Aluguel de Quadra</h2>
-      <div className="h-96 bg-white w-2/3 mx-auto grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div className="bg-white w-2/3 mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 lg:px-8">
 
-        <div className="bg-bege-800 rounded-lg h-96 p-4 text-white grid justify-items-stretch">
-          <h3 className="text-2x justify-self-start font-bold border-b w-full">ALUGUEL DE QUADRA</h3>
-          <div className="flex flex-col justify-center border-b">
+        <div className="bg-bege-800 rounded-lg h-96 p-4 text-white grid justify-items-stretch drop-shadow-md">
+          <h3 className="text-xl lg:text-2xl text-center font-bold border-b-2 w-full">ALUGUEL DE QUADRA</h3>
+          <div className="flex flex-col justify-center border-b-2">
+          <div className='flex items-center gap-2'>
+            <FaCheck className="text-lime-600"/>
             <p className="">1 HORA</p>
-            <p className="">1 VEZ POR SEMANA</p>
           </div>
-          <div>
-            <p className="">Ver dias e horarios disponiveis</p>
+          <div className='flex items-center gap-2'>
+            <FaCheck className="text-lime-600" />
+            <p className="">1 POR SEMANA</p>
+          </div>
+          </div>
+          <div className='flex flex-col items-center justify-center gap-4'>
+            <p className="text-center font-semibold">Ver dias e horarios disponiveis</p>
             <button className="py-4 px-2 bg-whatsapp text-white font-bold rounded-lg border">Entrar em contato </button>
           </div>
         </div>
-        <div className="bg-bege-100 rounded-lg h-96 p-4">
-          <h3 className="text-2x">AVULSO</h3>
-          <div>
-            <h3>DIUNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
+        
+        <div className="bg-bege-100 rounded-lg h-96 p-4 text-bege-800 grid justify-items-stretch drop-shadow-md">
+          <h3 className="text-xl lg:text-2xl text-center font-bold border-b-2 border-bege-800 w-full">AVULSO</h3>
+          <div className="flex flex-col justify-evenly border-b-2 border-bege-800">
+            <h3 className='font-bold text-center lg:text-xl'>DIURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>1 VEZ</p>
+              </div>
+              <p className="font-bold">R$ 180,00</p>
+            </div>
           </div>
-          <div>
-            <h3>NOTURNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
-          </div>
-        </div>
-        <div className="bg-bege-100 rounded-lg h-96 p-4">
-          <h3 className="text-2x">AVULSO</h3>
-          <div>
-            <h3>DIUNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
-          </div>
-          <div>
-            <h3>NOTURNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
+          <div className="flex flex-col justify-evenly">
+            <h3 className='font-bold text-center lg:text-xl'>NOTURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>1 VEZ</p>
+              </div>
+              <p className="font-bold">R$ 180,00</p>
+            </div>
           </div>
         </div>
-        <div className="bg-bege-100 rounded-lg h-96 p-4">
-          <h3 className="text-2x">AVULSO</h3>
-          <div>
-            <h3>DIUNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
+        <div className="bg-bege-100 rounded-lg h-96 p-4 text-bege-800 grid justify-items-stretch drop-shadow-md">
+          <h3 className="text-xl lg:text-2xl text-center font-bold border-b-2 border-bege-800 w-full">MENSAL</h3>
+          <div className="flex flex-col justify-evenly border-b-2 border-bege-800">
+            <h3 className='font-bold text-center lg:text-xl'>DIURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>1H POR SEMANA</p>
+              </div>
+              <p className="font-bold">R$ 460,00</p>
+            </div>
           </div>
-          <div>
-            <h3>NOTURNO</h3>
-              <p className="">1 HORA</p>
-              <p className="">1 VEZ</p>
-              <p className="">R$ 180,00</p>
+          <div className="flex flex-col justify-evenly">
+          <h3 className='font-bold text-center lg:text-xl'>NOTURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>1H POR SEMANA</p>
+              </div>
+              <p className="font-bold">R$ 460,00</p>
+            </div>
           </div>
         </div>
+        <div className="bg-bege-100 rounded-lg h-96 p-4 text-bege-800 grid justify-items-stretch drop-shadow-md">
+          <h3 className="text-xl lg:text-2xl text-center font-bold border-b-2 border-bege-800 w-full">MENSAL</h3>
+          <div className="flex flex-col justify-evenly border-b-2 border-bege-800">
+            <h3 className='font-bold text-center lg:text-xl'>DIURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>2H POR SEMANA</p>
+              </div>
+              <p className="font-bold">R$ 660,00</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-evenly">
+            <h3 className='font-bold text-center lg:text-xl'>NOTURNO</h3>
+            <div className='flex flex-col gap-2'>
+              <div className='flex items-center gap-2'>
+                <FaCheck className="text-lime-600" />
+                <p>2H POR SEMANA</p>
+              </div>
+              <p className="font-bold">R$ 660,00</p>
+            </div>
+          </div>
+        </div>  
+        
+
+      
         
       </div>
 
